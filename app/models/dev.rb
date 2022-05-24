11c07bdfffd90a120e621474089310d71ed75773
Patrick Hart <pathart612@gmail.com>
2022-05-24 11:39:26 -0400

    commit tree e8dfd4ddec16977de32d5c02ce996cd595f20f5a

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,21 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        if self.freebies.find_by(item_name: item_name)
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if self.freebies.find_by(dev_id: self.id)
+            freebie.update(dev_id: dev.id)
+            "#{self.name} gave away a freebie!"
+        else
+            "#{self.name} doesn't own that freebie!"
+        end
+    end
 end
